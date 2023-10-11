d2652dfb5ec532ba8ae201f7501035832c69cb35
Matheus <484471679@qq.com>
2023-10-11 23:39:40 +0800

    add method: return the size of list using recursion

--- src/lists/list/IntList.java
+++ src/lists/list/IntList.java
@@ -13,9 +13,22 @@ public class IntList {
         rest = r;
     }
 
+    /**
+     * return the size of the list
+     * @return
+     */
+    public int size(){
+        if (rest == null){
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
     public static void main(String[] args) {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
         L = new IntList(5, L);
+
+        System.out.println("L.size() = " + L.size());
     }
 }
